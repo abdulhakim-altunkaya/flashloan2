ac36a5a7d6f59dbead22c71c93e15f8bf25b8821
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-07-23 00:37:36 +0300

    getPricemount test block iss ssuccessful

--- test/test.swap.js
+++ test/test.swap.js
@@ -19,5 +19,25 @@ describe("creaate contraact", () => {
   contractRouter = new ethers.Contract(addressRouter, routerABI, provider);
   contractToken = new ethers.Contract(addressFrom, erc20ABI, provider);
 
-  console.log(contractFactory);
+  it("connects to a provider, contractFactory, router, token", () => {
+    assert(provider._isProvider);
+    expect(contractFactory.address).to.equals("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
+  });
+
+    //get price informaation
+  const getAmountOut = async () => {
+    decimals = await contractToken.decimals();
+    const amountInHuman = "1";
+    amountIn = ethers.utils.parseUnits(amountInHuman, decimals).toString();
+    const amountsOut = await contractRouter.getAmountsOut(amountIn, [
+        addressFrom,
+        addressTo,
+    ]);
+    return amountsOut[1].toString();
+  }
+  it("getss the price of aamountsOut", async () => {
+    const amount = await getAmountOut();
+    assert(amount.toString())
+    console.log(amount);
+  })
 })
